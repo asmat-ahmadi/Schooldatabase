7e3e38f2021d497770c2b3365e8861f8b24c4484
asmat-ahmadi <afg.habib001@gmail.com>
2023-11-14 10:55:29 -0500

    This commit has teacher and student classes

---
+++ src/test/java/Class01/Students.java
@@ -0,0 +1,9 @@
+package Class01;
+
+public class Students {
+    public static void main(String[] args) {
+        String name ="ajmal";
+        String lastname = "omid";
+        String Class = "firstgrade";
+    }
+}
---
+++ src/test/java/Class01/Teacher.java
@@ -0,0 +1,12 @@
+package Class01;
+
+public class Teacher {
+    public static void main(String[] args) {
+        String name = "Habib";
+        String lastname = "Ahmadi";
+        int salary = 1000;
+    }
+
+
+
+}
